03d76c5bda14ee72d43c1718f7adb030e66de3f2
heronlancellot <heronlancellot@id.uff.br>
2025-06-13 15:45:22 -0300

    update: lib

---
+++ src/hooks/useTokensList.ts
@@ -0,0 +1,35 @@
+import useSWR from "swr";
+import { TokenList } from "@/components/TokenSelector/types/token";
+import { TOKEN_LIST_URL } from "@/lib/constants/tokenList";
+
+const fetchTokenList = async () => {
+  try {
+    const response = await fetch(TOKEN_LIST_URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const tokensList: TokenList[] = data.assets;
+    return tokensList;
+  } catch (error) {
+    console.error("Error fetching token list:", error);
+    throw error;
+  }
+};
+
+export const useTokensList = () => {
+  const { data, error, isLoading } = useSWR("token-list", fetchTokenList, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    dedupingInterval: 60000,
+    refreshInterval: 300000,
+    errorRetryCount: 3,
+    shouldRetryOnError: true,
+  });
+
+  return {
+    tokensList: data || [],
+    isLoading,
+    isError: error,
+  };
+};
---
+++ src/lib/constants/tokenList.ts
@@ -0,0 +1,2 @@
+export const TOKEN_LIST_URL =
+  "https://raw.githubusercontent.com/soroswap/token-list/refs/heads/main/tokenList.json";
