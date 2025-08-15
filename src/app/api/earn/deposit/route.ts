37a966a8d6e67341aa8cdd1a4b730ab079e775a7
heronlancellot <heronlancellot@gmail.com>
2025-08-15 16:20:28 -0300

    update: number - deposit

--- src/app/api/earn/deposit/route.ts
+++ src/app/api/earn/deposit/route.ts
@@ -49,7 +49,7 @@ export async function POST(request: NextRequest) {
     const data = await defindexClient.depositToVault(
       vaultId,
       {
-        amounts: [BigInt(amount)],
+        amounts: [Number(amount)],
         caller: caller,
         slippageBps: Number(slippageBps),
         invest: true,
