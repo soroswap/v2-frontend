832d84803a462787096cf395258dc34ead500c40
heronlancellot <heronlancellot@gmail.com>
2025-08-15 21:40:37 -0300

    update: route.ts

---
+++ src/app/api/earn/withdraw/route.ts
@@ -0,0 +1,53 @@
+import { defindexClient } from "@/shared/lib/server/defindexClient";
+import { SupportedNetworks } from "@defindex/sdk";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { vaultId, network, amount, caller, slippageBps } = body;
+
+    if (!slippageBps) {
+      return NextResponse.json(
+        { error: "slippageBps is required" },
+        { status: 400 },
+      );
+    }
+
+    if (!caller) {
+      return NextResponse.json(
+        { error: "caller is required" },
+        { status: 400 },
+      );
+    }
+
+    if (!amount) {
+      return NextResponse.json(
+        { error: "amount is required" },
+        { status: 400 },
+      );
+    }
+
+    if (!vaultId || !network) {
+      return NextResponse.json(
+        { error: "vaultId and network are required" },
+        { status: 400 },
+      );
+    }
+
+    const data = await defindexClient.withdrawFromVault(
+      vaultId,
+      {
+        amounts: [Number(amount)],
+        caller: caller,
+        slippageBps: Number(slippageBps),
+      },
+      network as SupportedNetworks,
+    );
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error withdrawing from vault:", error);
+    return NextResponse.json(error);
+  }
+}
