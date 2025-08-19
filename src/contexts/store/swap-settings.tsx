df2f625a63f84fbf5ae03ac2302227bc13fe7224
heronlancellot <heronlancellot@gmail.com>
2025-08-18 23:42:38 -0300

    feat: save slippage data in storage

--- src/contexts/store/swap-settings.tsx
+++ src/contexts/store/swap-settings.tsx
@@ -1,16 +1,24 @@
 import { SwapSettings } from "@/features/swap/types";
 import { DEFAULT_SWAP_SETTINGS } from "@/shared/lib/constants/swap";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface SwapSettingsStore {
   swapSettings: SwapSettings;
   setSwapSettings: (partial: Partial<SwapSettings>) => void;
 }
 
-export const useSwapSettingsStore = create<SwapSettingsStore>((set) => ({
-  swapSettings: DEFAULT_SWAP_SETTINGS,
-  setSwapSettings: (partial) =>
-    set((state) => ({
-      swapSettings: { ...state.swapSettings, ...partial },
-    })),
-}));
+export const useSwapSettingsStore = create<SwapSettingsStore>()(
+  persist(
+    (set) => ({
+      swapSettings: DEFAULT_SWAP_SETTINGS,
+      setSwapSettings: (partial) =>
+        set((state) => ({
+          swapSettings: { ...state.swapSettings, ...partial },
+        })),
+    }),
+    {
+      name: 'swap-settings-storage',
+    }
+  )
+);
