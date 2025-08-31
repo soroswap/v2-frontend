2becb62a5392baf42fd9a431430833a1c1d622dd
heronlancellot <heronlancellot@gmail.com>
2025-08-31 13:26:29 -0300

    feat: add index & update import

--- src/shared/lib/utils/bigIntReplacer.ts
+++ src/shared/lib/utils/bigIntReplacer.ts
@@ -1,4 +1,6 @@
-// BigInt replacer function to convert BigInt values to strings
+/**
+ * BigInt replacer function to convert BigInt values to strings
+ */
 export const bigIntReplacer = (key: string, value: unknown): unknown => {
   if (typeof value === "bigint") {
     return value.toString();
