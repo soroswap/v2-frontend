49aadbba33ba1b5b731010af8f39d4db8d342326
coderipper <hello@joaquinsoza.dev>
2025-09-15 19:37:37 -0300

    hotfix: hided cetes and ustry

--- src/features/earn/constants/vault.ts
+++ src/features/earn/constants/vault.ts
@@ -2,23 +2,23 @@ import { RiskLevel } from "@/features/earn/types/RiskLevel";
 
 export const VAULT_MOCK: { vaultAddress: string; riskLevel: RiskLevel }[] = [
   {
-    vaultAddress: "CDONBLOOTYZ7QN62ZLJFHK7CT3JCP3JEZDCRSG3VLGAP73QAXS7HF6HU",
+    vaultAddress: "CDONBLOOTYZ7QN62ZLJFHK7CT3JCP3JEZDCRSG3VLGAP73QAXS7HF6HU", // XLM-BORING
     riskLevel: "low",
   },
   {
-    vaultAddress: "CA2FIPJ7U6BG3N7EOZFI74XPJZOEOD4TYWXFVCIO5VDCHTVAGS6F4UKK",
+    vaultAddress: "CA2FIPJ7U6BG3N7EOZFI74XPJZOEOD4TYWXFVCIO5VDCHTVAGS6F4UKK", // USDC Soroswap Earn BLEND
     riskLevel: "low",
   },
   {
-    vaultAddress: "CCKTLDG6I2MMJCKFWXXBXMA42LJ3XN2IOW6M7TK6EWNPJTS736ETFF2N",
+    vaultAddress: "CCKTLDG6I2MMJCKFWXXBXMA42LJ3XN2IOW6M7TK6EWNPJTS736ETFF2N", // EURC Soroswap Earn BLEND
     riskLevel: "low",
   },
-  {
-    vaultAddress: "CBHB2G4TMSVWE4YFDTFYRYNCP5KUT6RQVWQGIM4LQO2IKKHVDB7N5JJQ",
-    riskLevel: "low",
-  },
-  {
-    vaultAddress: "CCV43DIK4TLUHFKNE3XL4QMNU6P7EGKMYNCDFRIB4HX4FTRM2IBDQEPS",
-    riskLevel: "low",
-  },
+  // {
+  //   vaultAddress: "CBHB2G4TMSVWE4YFDTFYRYNCP5KUT6RQVWQGIM4LQO2IKKHVDB7N5JJQ", // CETES Soroswap Earn BLEND
+  //   riskLevel: "low",
+  // },
+  // {
+  //   vaultAddress: "CCV43DIK4TLUHFKNE3XL4QMNU6P7EGKMYNCDFRIB4HX4FTRM2IBDQEPS", // USTRY Soroswap Earn BLEND
+  //   riskLevel: "low",
+  // },
 ];
