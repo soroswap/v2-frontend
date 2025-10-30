fca29dcbe88fe16c4f4669a1c72fc5833641b1d2
akbarsaputrait <akbarsaputrait@outlook.com>
2025-10-30 18:04:12 +0700

    revert: bring back un-related changes

--- src/app/pools/add-liquidity/[...tokens]/page.tsx
+++ src/app/pools/add-liquidity/[...tokens]/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
- 
-"use client";
+ "use client";
 
 import { useState, useCallback, MouseEvent, useEffect } from "react";
 import {
--- src/shared/components/buttons/TheButton.tsx
+++ src/shared/components/buttons/TheButton.tsx
@@ -18,6 +18,7 @@ export const TheButton = ({
         "bg-brand hover:bg-brand/80 text-primary disabled:bg-surface-alt relative flex h-14 w-full cursor-pointer items-center justify-center rounded-2xl p-4 text-[20px] font-bold disabled:cursor-default disabled:text-[#6d7179] dark:disabled:bg-[#2e303b]",
         className,
       )}
+      disabled={props.disabled}
       {...props}
     >
       {children}
